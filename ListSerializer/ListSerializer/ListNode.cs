b75b3a9cd9a8f676d7deee7fe631cf5a5ce59ceb
Nikolay Bobovnikov <nbobovnikov@gmail.com>
2023-03-13 20:08:56 +0300

    wip

--- ListSerializer/ListSerializer/ListNode.cs
+++ ListSerializer/ListSerializer/ListNode.cs
@@ -4,9 +4,46 @@ using System.Drawing;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Xml.Linq;
 
 namespace ListSerializer
 {
+    public static class Extensions
+    {
+        public static void Write<T>(this Stream stream, T value)
+            where T : unmanaged
+        {
+            int size = Marshal.SizeOf(value);
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+
+            try
+            {
+                Marshal.StructureToPtr(value, ptr, false);
+                byte[] managedArray = new byte[size];
+                Marshal.Copy(ptr, managedArray, 0, size);
+                stream.Write(managedArray, 0, size);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+        }
+
+        public static T? Read<T>(this Stream stream)
+            where T : unmanaged
+        {
+            T? result = default;
+            int size = Marshal.SizeOf(result);
+            byte[] managedArray = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            stream.Read(managedArray, 0, size);
+            Marshal.Copy(managedArray, 0, ptr, size);
+            result = Marshal.PtrToStructure<T>(ptr);
+
+            return result;
+        }
+    }
 
     public class ListNode
     {
@@ -14,11 +51,51 @@ namespace ListSerializer
         public ListNode Next;
         public ListNode Random;
         public string Data;
-    }
 
-    public struct ListNodeData
-    {
-        public string Data;
+
+        public static void Serialize(ListNode? node, Stream s)
+        {
+            if (node != null)
+            {
+                s.Write(node);
+
+                if (node.Previous != null)
+                {
+                    Serialize(node.Previous, s);
+                }
+                if (node.Next != null)
+                {
+                    Serialize(node.Next, s);
+                }
+                if (node.Random != null)
+                {
+                    Serialize(node.Random, s);
+                }
+            }
+        }
+
+        public static ListNode? Deserialize(ListNode? node, Stream s)
+        {
+            var n = s.Read<ListNode>();
+
+            if (node != null)
+            {
+                if (node.Previous != null)
+                {
+                    Serialize(node.Previous, s);
+                }
+                if (node.Next != null)
+                {
+                    Serialize(node.Next, s);
+                }
+                if (node.Random != null)
+                {
+                    Serialize(node.Random, s);
+                }
+            }
+
+            return node;
+        }
     }
 
     public class ListRandom
@@ -29,34 +106,28 @@ namespace ListSerializer
 
         public void Serialize(Stream s)
         {
-            //var v = default(ListNodeData);
-            var v = new ListNodeData() { Data = "Hello" };
-            int size = Marshal.SizeOf(v);
-            IntPtr ptr = Marshal.AllocHGlobal(size);
+            s.Write(Count);
 
-            try
+            if (Head != null)
             {
-                Marshal.StructureToPtr(v, ptr, false);
-                byte[] managedArray = new byte[size];
-                Marshal.Copy(ptr, managedArray, 0, size);
-                s.Write(managedArray, 0, size);
+                ListNode.Serialize(Head, s);
             }
-            finally
+
+            if (Tail != null)
             {
-                // Free the unmanaged memory.
-                Marshal.FreeHGlobal(ptr);
+                ListNode.Serialize(Tail, s);
             }
         }
 
         public void Deserialize(Stream s)
         {
-            var v = default(ListNodeData);
-            int size = Marshal.SizeOf(v);
-            byte[] managedArray = new byte[size];
-            IntPtr ptr = Marshal.AllocHGlobal(size);
-            s.Read(managedArray, 0, size);
-            Marshal.Copy(managedArray, 0, ptr, size);
-            v = Marshal.PtrToStructure<ListNodeData>(ptr);
+            if (s.Read<int>() is int count)
+            {
+                Count = count;
+            }
+
+            ListNode.Deserialize(Head, s);
+            ListNode.Deserialize(Tail, s);
         }
     }
 
--- ListSerializer/ListSerializer/Program.cs
+++ ListSerializer/ListSerializer/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
-            ListRandom s = new ListRandom();
-            using (Stream stream = new FileStream("log", FileMode.CreateNew))
+            var head = new ListNode() { Data = "HeadData" };
+            var tail = new ListNode() { Data = "TailData" };
+            ListRandom s = new ListRandom() { Count = 123, Head = head, Tail = tail };
+
+            using (Stream stream = new FileStream("log", FileMode.Create))
             {
                 s.Serialize(stream);
             }
